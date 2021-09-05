4e34280930e80dd1b9fc98d592eff2ed592a2d3c
박해민 <phm9368@sookmyung.ac.kr>
2021-09-05 11:48:49 +0900

    [Feat] Use MotionLayout

--- app/src/main/java/com/example/loadapp/DetailActivity.kt
+++ app/src/main/java/com/example/loadapp/DetailActivity.kt
@@ -1,15 +1,39 @@
 package com.example.loadapp
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_detail.*
+import androidx.databinding.DataBindingUtil
+import com.example.loadapp.databinding.ActivityDetailBinding
 
 class DetailActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDetailBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
-        setSupportActionBar(toolbar)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_detail)
+        binding.lifecycleOwner = this
+
+        setSupportActionBar(binding.toolbar)
+
+        val fileName = intent.getStringExtra("fileName")
+        val fileStatus = intent.getBooleanExtra("downloadStatus", false)
+
+        binding.contentDetail.tvFileName.text =
+            String.format(resources.getString(R.string.file_name_format), fileName)
+        var status = ""
+        if (fileStatus) {
+            status = "Success"
+        } else {
+            status = "Fail"
+            binding.contentDetail.tvFileStatus.setTextColor(Color.RED)
+        }
+        binding.contentDetail.tvFileStatus.text =
+            String.format(resources.getString(R.string.file_status_format), status)
+
+        binding.contentDetail.btnOk.setOnClickListener {
+            finish()
+        }
     }
 
 }
